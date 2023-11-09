0b9cd94e3004fb244b0937926ea1d0bbe92bcc45
BreakingDev24 <102031294+BreakingDev24@users.noreply.github.com>
2023-11-10 00:29:13 +0100

    push book in array

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,3 +15,35 @@ newBookBtn.addEventListener('click', () =>{
 closeDialogBtn.addEventListener('click', () => {
     formContainerDialog.close()
 })
+
+formContainerDialog.showModal()
+
+// LIBRARY
+const myLibrary = [];
+
+// construnctor function for book
+
+function Book(title, author, pages, read){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+function addBookToLibrary(title, author, pages) {
+    let newBook = new Book(title, author, pages)
+    myLibrary.push(newBook)
+}
+
+function getBookValue(e){
+    e.preventDefault()
+
+    const titleValue = document.getElementById('title').value;
+    const authorValue = document.getElementById('author').value;
+    const pagesValue = document.getElementById('pages').value;
+    
+    addBookToLibrary(titleValue, authorValue, pagesValue)
+    console.log(myLibrary);
+}
+
+subtmitBtn.addEventListener('click', getBookValue)
